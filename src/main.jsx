000edf6734adaa2c3ad4f354ff32388e86d5df30
Abdullahbdbd <121636151+Abdullahbdbd@users.noreply.github.com>
2025-10-08 12:38:38 +0600

    Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1 className='text-5xl'>abdullah</h1>,
+  },
+]);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />,
+  </StrictMode>,
+)
